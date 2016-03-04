33c0ee2d87c526f928158014426123690051a185
Louisa Reese <louisa.e.reese@gmail.com>
2016-03-04 15:17:22 -0700

    started email validation bullshit

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -24,25 +24,98 @@ router.post('/', validEmail, function(req, res, next) {
 
 // helpers
 
-function validEmail(req, res, next){
-	 if( email.indexOf(req.body.email) === -1 ){ // if not there
-	 	// throw error
-	 	// send flash message
-	 	req.flash('message',{
-	 	 status: 'danger',
-	 	 value:'Invalid email, please try again'
-	 	});
-	 	res.redirect('/');
-	 	} else {
-	 	// pass request to callback function
-	 	req.flash('message',{
-	 			status: 'success',
-	 			value: 'Valid email'
-	});
-		return next();
-	 }
+// function validEmail(req, res, next){
+// 	/* not black
+// 	not just @ symbol
+// 	text before @
+// 	valid text after
+// 	*/
 
-}
+// var email = req.body.email;
+//  if ( !email.trim() ) {
+//    req.flash('message', {
+//      status: 'danger',
+//      value: 'Invalid Email.  Please try again.'
+//    });
+//    return res.redirect('/');
+//  }
+
+//  if (email === '@') {
+//    req.flash('message', {
+//      status: 'danger',
+//      value: 'Invalid Email.  Please try again.'
+//    });
+//    return res.redirect('/');
+//  }
+
+
+// 	if( email.indexOf('@') === -1 ){
+// 	req.flash('message', {
+// 		status: 'danger',
+// 		value: 'Invalid Email.  Please try again.'
+// 	});
+// }
+
+// var emailArray = email.split('@');
+// if( emailArray.length !== 2 && emailArray[0].length <= 0 || emailArray[1].length <= 0 ){
+// 	req.flash('message', {
+// 		status: 'danger',
+// 		value: 'Invalid email please try again.'
+// 	})
+
+// var afterAtArray = emailArray[1].split('.');
+
+// 	if( afterAtArray.length !== 2 && emailArray[0].length === 3){
+
+// 	}
+
+// }
+
+// return next();
+
+// 	//  if( email.indexOf(req.body.email) === -1 ){ // if not there
+// 	//  	// throw error
+// 	//  	// send flash message
+// 	//  	req.flash('message',{
+// 	//  	 status: 'danger',
+// 	//  	 value:'Invalid email, please try again'
+// 	//  	});
+// 	//  	res.redirect('/');
+// 	//  	} else {
+// 	//  	// pass request to callback function
+// 	//  	req.flash('message',{
+// 	//  			status: 'success',
+// 	//  			value: 'Valid email'
+// 	// });
+// 	// 	return next();
+// 	//  }
+
+// }
+
+
+
+
+// function validateEmail (email) {
+//     var emailArray = email;
+//     var emailArraySplit = email.split('@');
+//     console.log(emailArray);
+//     if (emailArray === "") {
+//       console.log("No email provided, returns false");
+//       return false;
+//     }
+//     else if (emailArraySplit[1].indexOf('.') === -1) {
+//       console.log("Missing a '.' after the '@', returns false");
+//       return false;
+//     }
+//     else {
+//       console.log("Email is valid, returns true");
+//       return true;
+//     }
+//   }
+
+
+
+
 
 
 module.exports = router;
